03ea84a8c181b98f5d7f4ae5df7e9eba3e9fa5c1
coker2019 <olabisi2k11@gmail.com>
2023-05-12 16:29:34 +0100

    Implement add/remove function.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,15 @@
 import './style.css';
+import { createMyTask } from './modules/Manager.js';
 
-const taskContainer = document.querySelector('.all-tasks');
-const tasks = [
-  {
-    description: 'Make Breakfast',
-    completed: false,
-    id: 0,
+const createTask = document.getElementById('create');
+const form = document.querySelector('form');
+const formInput = document.querySelector('.input');
 
-  },
-  {
-    description: 'Clean the house',
-    completed: false,
-    id: 1,
-  },
-  {
-    description: 'go to work',
-    completed: false,
-    id: 2,
-  },
-  {
-    description: 'Join stand up team',
-    completed: false,
-    id: 3,
-  },
-];
-
-tasks.forEach((task, e) => {
-  const taskItem = document.createElement('div');
-  taskItem.classList = 'list-item';
-
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-
-  const taskDesc = document.createElement('input');
-  taskDesc.type = 'text';
-  taskDesc.value = `${tasks[e].description}`;
-
-  const ellipses = document.createElement('button');
-  ellipses.innerHTML = '<i class="uil uil-ellipsis-v"></i>';
-
-  taskItem.append(checkbox);
-  taskItem.append(taskDesc);
-  taskItem.append(ellipses);
-
-  taskContainer.append(taskItem);
-  taskDesc.removeAttribute('disabled');
-  taskDesc.focus();
+createTask.addEventListener('click', (e) => {
+  e.preventDefault();
+  const myTask = formInput.value;
+  if (myTask) {
+    createMyTask();
+    form.reset();
+  }
 });
